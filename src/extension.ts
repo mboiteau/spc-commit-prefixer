2a469f2d6915f9a9ba4a950cf15e1f922fc20aa8
Maxime-Boiteau_tvlport <maxime.boiteau@travelport.com>
2023-04-13 23:50:49 +0200

    feat(spc-commit-prefixer): create extension

---
+++ src/extension.ts
@@ -0,0 +1,63 @@
+import * as vscode from "vscode";
+import { GitExtension, Repository } from "./api/git";
+
+let commitMessage: string;
+
+function getGitExtension() {
+  const vscodeGit = vscode.extensions.getExtension<GitExtension>("vscode.git");
+  const gitExtension = vscodeGit && vscodeGit.exports;
+  return gitExtension && gitExtension.getAPI(1);
+}
+
+async function buildCommit(repository: Repository) {
+  const { pluginName, commitPrefixes } =
+    vscode.workspace.getConfiguration("SPC-CommitPrefixer");
+  const selectedPrefix = await vscode.window.showQuickPick(commitPrefixes, {
+    placeHolder: "Select a prefix that you want to apply to your commit",
+  });
+
+  if (selectedPrefix) {
+    const userCommitInput = await vscode.window.showInputBox({
+      title: "Commit Message",
+      placeHolder: "Enter your commit message",
+      prompt: "Enter your commit message",
+    });
+    commitMessage = `${selectedPrefix}(${pluginName}): ${userCommitInput}`;
+  }
+  repository.inputBox.value = commitMessage;
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  const disposables: vscode.Disposable[] = [];
+  const showPrefixPopup = vscode.commands.registerCommand(
+    "spc-commit-prefixer.showPrefixPopup",
+    async (uri?) => {
+      const git = getGitExtension();
+
+      if (!git) {
+        vscode.window.showErrorMessage("Unable to load Git Extension");
+        return;
+      }
+
+      vscode.commands.executeCommand("workbench.view.scm");
+
+      if (uri) {
+        const selectedRepository = git.repositories.find((repository) => {
+          return repository.rootUri.path === uri.rootUri.path;
+        });
+
+        if (selectedRepository) {
+          await buildCommit(selectedRepository);
+        }
+      } else {
+        for (const repo of git.repositories) {
+          await buildCommit(repo);
+        }
+      }
+    }
+  );
+  disposables.push(showPrefixPopup);
+  context.subscriptions.push(...disposables);
+}
+
+export function deactivate() {}
